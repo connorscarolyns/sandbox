ec164ff76c44eec5937264af8088221b05189dd0
carolyn <connorscarolyns@gmail.com>
2023-04-17 11:17:36 -0300

    modal experiment
    first commit

---
+++ modal-experiment/src/index.js
@@ -0,0 +1,12 @@
+import { Carousel, initTE } from "tw-elements";
+initTE({ Carousel });
+
+// Initialization for ES Users
+import {
+    Modal,
+    Ripple,
+    initTE,
+  } from "tw-elements";
+  
+  initTE({ Modal, Ripple });
+
